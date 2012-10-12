0006ae3601f54c88ae03f3c89e0f29491ad0e366
mikeycgto <mikeycgto@gmail.com>
2012-10-11 20:57:54 -0400

    update views and assets; add latest route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Apod2::Application.routes.draw do
   root controller: 'pictures', action: 'index'
 
-  resources :pictures, only: [:index, :show]
+  resources :pictures, only: [:index, :show] do
+    get 'latest', on: :collection
+  end
 end
