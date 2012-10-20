9ae561541ff5eddd87cc16627bec757b1cd7e4ea
mikeycgto <mikeycgto@gmail.com>
2012-10-20 11:57:45 -0400

    update Picture model

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -3,7 +3,9 @@ class Picture < ActiveRecord::Base
 
   validates :date, presence: true
 
-  scope :latest, order('date DESC')
+  scope :latest, lambda {
+    order('date DESC')
+  }
 
   def next_picture
     self.class.where('date > ?', date).order('date ASC').first
@@ -18,6 +20,26 @@ class Picture < ActiveRecord::Base
   end
 
   def to_param
+    # update for year >=2095
     date.strftime 'ap%y%m%d'
+  end
+
+  alias :apid :to_param
+
+  def serializable_hash(options = {})
+    super options.merge!({
+      only: [:title, :date, :media],
+      methods: [:apid]
+    })
+  end
+
+  def self.date_from_apid(id)
+    parts = id.scan(%r[\d{2}]).map(&:to_i)
+
+    if parts.size == 3
+      parts[0] = parts[0] + (parts[0] >= 95 ? 1900 : 2000)
+
+      Date.new(*parts) rescue nil
+    end
   end
 end
