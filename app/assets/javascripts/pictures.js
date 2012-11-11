e7a44703b6f258c02a4241f50e919f8f1ad0ac55
mikeycgto <mikeycgto@gmail.com>
2012-11-11 17:09:51 -0500

    frontend stuff

--- app/assets/javascripts/pictures.js
+++ app/assets/javascripts/pictures.js
@@ -1,31 +1,33 @@
 $(function(){
-	var win = $(window);
-	var body = document.body;
+  var win = $(window);
+  var body = $('body');
 
-	var pictures = $('#pictures');
-	var nomore = false;
+  var pictures = $('#pictures');
+  var nomore = false;
 
-	function load(pics){
-		if (pics.length === 0)
-			nomore = true;
+  function load(pics){
+    if (pics.length === 0)
+      nomore = true;
 
-		for (var i = 0, pic; pic = pics[i]; i++){
-			var href = '/pictures/'+ pic.apid;
-			var tmpl = template('picture-tmpl', {
-				href: href, media: pic.media || 'No Image'
-			});
+    for (var i = 0, pic; pic = pics[i]; i++){
+      var href = '/pictures/'+ pic.apid;
+      var tmpl = template('picture-tmpl', {
+        href: href, media: pic.media || 'No Image'
+      });
 
-			pictures.append(tmpl);
-		}
-	}
+      pictures.append(tmpl);
+    }
+  }
 
-	$(window).on('scroll', function(ev){
-		if (body.scrollHeight - win.scrollTop() > win.height()) return;
-		if (nomore) return;
+  $(window).on('scroll', function(ev){
+    if (body.height() <= win.height() + win.scrollTop()) return;
+    if (nomore) return;
 
-		var last = pictures.children().last().find('a');
-		var apid = last.attr('href').split('/').pop();
+    console.log('scroll');
 
-		$.get('/pictures', { last: apid }, load, 'json');
-	});
+    var last = pictures.children().last().find('a');
+    var apid = last.attr('href').split('/').pop();
+
+    $.get('/pictures', { last: apid }, load, 'json');
+  });
 });
